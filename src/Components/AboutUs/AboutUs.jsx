21ba8e12609dabc53a1d7eab6b5826d760cb937e
Kishore_ML <kishore_ml@trimble.com>
2025-08-20 21:15:44 +0530

    UI Correction

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -11,29 +11,28 @@ const AboutUs = () => {
             Ashwin’s Academy is your trusted partner in pursuing master’s programs in Germany. We combine first-hand experience with expert guidance to make your academic journey abroad seamless and stress-free.
             <br></br>
             <br></br>
+
             <b>🎯 Our Mission</b>
             <br></br>
             To empower students with the right guidance, tools, and strategies to secure admissions in top German universities — without the confusion or guesswork.
             <br></br>
             <br></br>
 
-            
             <b>🌍 Why Choose Us</b>
-            <br></br>
             <br></br>
             Personalized Guidance: Every student’s journey is unique, and so is our approach.
             <br></br>
             End-to-End Support: From SOPs to Visa — we’ve got you covered.
             <br></br>
             Transparent Process: No hidden steps, no surprises.
-
-
             <br></br>
             <br></br>
+
             <b>📈 Our Promise</b>
             <br></br>
                 We focus on delivering clarity, accuracy, and success at every stage of your admission process. With Ashwin’s Academy, you’re not just applying to universities — you’re building your future with confidence.
             </p>
+            
             </div>
         </div>
     )
